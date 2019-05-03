a3c4341763f2e13c6cac2f065715378557c367c3
Fernando1509 <48602529+Fernando1509@users.noreply.github.com>
2019-05-02 23:10:57 -0500

    6

--- acciones.js
+++ acciones.js
@@ -4,12 +4,12 @@ $(document).ready(funtion(e){
 	document.addEventListener("deviceready" , function(){
 		
 		$('#beep').tap(fuction(){
-			navigator.notification.beep(1);
-		});//tap beep
+			navigator.notification.Beep(1);
+		});//tap Beep
 		
 		$('#vibrar').tap(fuction(){
-			navigator.notification.beep(1000);
-		});//tap vibrar
+			navigator.notification.Vibrate(1000);
+		});//tap Vibrar
 		
 	},false); //deviceready
 }); //ready
